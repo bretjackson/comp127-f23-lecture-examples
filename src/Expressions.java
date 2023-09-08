50ce888731de20a8f66a4b17d9854ae4169104ca
bretjackson <bjackson@macalester.edu>
2023-09-08 10:24:41 -0500

    Added expression example

---
+++ src/Expressions.java
@@ -0,0 +1,14 @@
+public class Expressions {
+
+    public static void main(String[] args) {
+        int age; //declare a variable
+        age = 2; //assignment
+
+        /*
+         * This is a multi-line comment
+         * See!
+         */
+        int sonAge = 4;
+        System.out.println("My daughter is "+age+" years old.");
+    }
+}
